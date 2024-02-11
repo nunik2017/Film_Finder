dd6f8b0afc52eb778152ea66adc8d67afe0a719b
Nejc <nejc@Nejcs-MacBook-Pro.local>
2024-02-11 19:20:04 +0100

    adding more comments

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 import * as helpers from './helpers.js';
 
+//Saving API key from TMDB API
 const tmdbKey = 'a995f12d420a59b5ff6e47ee8cdabab9';
-const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3'; //Base Url for all API cals
 const playBtn = document.getElementById('playBtn');
 
 require('dotenv').config();
 
 console.log(process.env);
 
+//API call for genres
 const getGenres = async () => {
- const genreRequestEndpoint = '/genre/movie/list';
- const requestParams = `?api_key=${tmdbKey}`;
- const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+ const genreRequestEndpoint = '/genre/movie/list'; // API endpoint
+ const requestParams = `?api_key=${tmdbKey}`; //Query string, to attach API key
+ const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`; //Generating full address
  try {
   const response = await fetch(urlToFetch);
-  if (response.ok) {
-    const jsonResponse = await response.json();
+  if (response.ok) { //Check if response is TRUEty
+    const jsonResponse = await response.json(); //Converting response object to JSON object
     console.log(jsonResponse);
-    const genres = jsonResponse.genres;
+    const genres = jsonResponse.genres; //Saving genres property of jsonResponse to var: genres
     return genres;
   }
- } catch (error) {
-  console.log(error);
+ } catch (error) { //Catch error
+  console.log(error); //Log error
  }
 };
 
 const getMovies = async () => {
   const selectedGenre = getSelectedGenre();
   const discoverMovieEndpoint = '/discover/movie';
-  const requestParams = `?api_key=${tmdbKey}&${selectedGenre}`;
+  const requestParams = `?api_key=${tmdbKey}&${selectedGenre}`; //
   const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
   try {
     const response = await fetch(urlToFetch);
